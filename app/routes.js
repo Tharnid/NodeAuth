239a1ab92d38e5ff177a55f8ff6f682ae759a00d
Tharnid <jeffmsmithdev@gmail.com>
2013-12-14 00:00:44 -0500

    passport facebook

--- app/routes.js
+++ app/routes.js
@@ -39,7 +39,24 @@ module.exports = function(app, passport) {
 		failureFlash : true // allow flash messages
 	}));
 
-	// facebook routes
+	// =====================================
+	// FACEBOOK ROUTES =====================
+	// =====================================
+	// route for facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	// route for logging out
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 
 	// =====================================
 	// TWITTER ROUTES ======================
